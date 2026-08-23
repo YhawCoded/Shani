849eedfac639e4b9f2cd7027d328f3de13b2bbeb
Yaw Agyiri <agyiriyaw.official@gmail.com>
2026-08-23 23:46:14 +0000

    Screen transitions

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
+const opening = document.getElementById("opening");
+const reveal = document.getElementById("reveal");
+
 const beginBtn = document.getElementById("beginBtn");
+const continueBtn = document.getElementById("continueBtn");
+
 
 beginBtn.addEventListener("click", () => {
-  beginBtn.innerHTML = "Loading something special... ✦";
+
   beginBtn.disabled = true;
 
+  opening.classList.add("fade-out");
+
   setTimeout(() => {
-    alert("Okay Shani... you made it this far 👀");
-  }, 900);
+
+    opening.classList.add("hidden");
+
+    reveal.classList.remove("hidden");
+    reveal.classList.add("fade-in");
+
+  }, 800);
+
+});
+
+
+continueBtn.addEventListener("click", () => {
+
+  continueBtn.disabled = true;
+
+  reveal.classList.add("fade-out");
+
+  setTimeout(() => {
+
+    // Next section coming soon 👀
+
+    reveal.classList.add("hidden");
+
+    alert("There's more... 👀");
+
+  }, 800);
+
 });
